2a0a56e099e7020b73dc74483c7ad45e63a7faf0
YannisPoder <ypoder@gaming.tech>
2026-09-17 13:40:10 +0200

    ADD
    sdf texture generator

---
+++ Assets/SDFTextureGenerator/Example/Assets/SDFTextureGeneratorExample.cs
@@ -0,0 +1,39 @@
+/*
+	Copyright © Carl Emil Carlsen 2021
+	http://cec.dk
+*/
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SDFTextureGeneratorExample : MonoBehaviour
+{
+	[SerializeField] Texture _sourceTexture = null;
+	[SerializeField] float _sourceValueThreshold = 0.5f;
+	[SerializeField] SDFTextureGenerator.DownSampling _downSampling = SDFTextureGenerator.DownSampling.None;
+	[SerializeField] SDFTextureGenerator.Precision _precision = SDFTextureGenerator.Precision._32;
+
+	[Header("Output")]
+	[SerializeField] UnityEvent<RenderTexture> _sdfTextureEvent = null;
+
+	SDFTextureGenerator _generator;
+
+
+	void OnEnable()
+	{
+		_generator = new SDFTextureGenerator();
+	}
+
+
+	void OnDisable()
+	{
+		_generator.Release();
+	}
+
+
+	void Update()
+	{
+		_generator.Update( _sourceTexture, _sourceValueThreshold, _downSampling, _precision );
+		_sdfTextureEvent.Invoke( _generator.sdfTexture );
+	}
+}
